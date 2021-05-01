89032c9a4ca9bfacd4b2c98eabfbc1c75cd21c69
stefzouille <contact.lokcy@gmail.com>
2021-05-01 15:54:26 +0200

    add

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
-import Coins from "../components/Coins";
+import CoinList from "../components/CoinList";
+//import Coins from "../components/Coins";
 import SearchBar from "../components/SearchBar";
 
 
-export default function Home() {
+export default function Home({ filteredCoins }) {
   return (
     <div>
       <Head>
@@ -13,11 +14,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SearchBar type="text" placeholder="Search"/>
-      <Coins />
+      <CoinList filteredCoins={filteredCoins} />
     </div>
   );
 }
 
 export const getServerSideProps = async () => {
   const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+
+  const filteredCoins = await res.json()
+
+    return {
+      props: {
+        filteredCoins
+      }
+    }
 }
