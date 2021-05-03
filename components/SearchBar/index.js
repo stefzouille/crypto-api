093a102ccbf3d4596b3a7eba9e6e2bd3036f50cd
stefzouille <contact.lokcy@gmail.com>
2021-05-03 09:27:21 +0200

    add

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,12 +1,11 @@
-import styles from './Search.module.css'
+import styles from './Search.module.css';
 
-
-const SearchBar = ({...rest}) => {
+const SearchBar = ({ ...rest }) => {
   return (
-  <div className={styles.coin_search}>
-    <input className={styles.coin_input}{...rest} />
-  </div>
-  )
+    <div className={styles.coin_search}>
+      <input className={styles.coin_input} {...rest} />
+    </div>
+  );
 };
 
 export default SearchBar;
